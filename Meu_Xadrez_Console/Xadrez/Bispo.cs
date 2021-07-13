edcaf198d36809bf0984d4b4a37f5ad1cd62a4b1
VicenteLamberti <vicente.lamberti@hotmail.com>
2021-07-13 20:46:20 -0300

    Implentacoes e posicionamento de todas as pecas

---
+++ Meu_Xadrez_Console/Xadrez/Bispo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabuleiroNameSpace;
+
+namespace XadrezNameSpace
+{
+    class Bispo:Peca
+    {
+        public Bispo(Tabuleiro tab, Cor cor):base(tab,cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tabuleiro.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+            Posicao pos = new Posicao(0, 0);
+            //NO
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if(Tabuleiro.peca(pos)!=null && Tabuleiro.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha - 1, pos.Coluna - 1);
+            }
+
+            //NE
+
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna +1 );
+            while (Tabuleiro.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.peca(pos) != null && Tabuleiro.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha - 1, pos.Coluna + 1);
+            }
+
+            //SE
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.peca(pos) != null && Tabuleiro.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha + 1, pos.Coluna + 1);
+            }
+
+
+            //SO
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.peca(pos) != null && Tabuleiro.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha + 1, pos.Coluna - 1);
+            }
+            return mat;
+        }
+    }
+}
---
+++ Meu_Xadrez_Console/Xadrez/Peao.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabuleiroNameSpace;
+
+namespace XadrezNameSpace
+{
+    class Peao : Peca
+    {
+        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+
+        
+
+        private bool existeInimigo(Posicao pos)
+        {
+            Peca p = Tabuleiro.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        private bool livre(Posicao pos)
+        {
+            return Tabuleiro.peca(pos) == null;
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+            Posicao pos = new Posicao(0, 0);
+            
+            if(Cor == Cor.Branca)
+            {
+                Posicao.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+                if(Tabuleiro.PosicaoValida(pos)&& livre(pos))
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+
+
+                Posicao.definirValores(Posicao.Linha - 2, Posicao.Coluna);
+                if (Tabuleiro.PosicaoValida(pos) && livre(pos) && QuantidadeMovimentos==0)
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+
+                Posicao.definirValores(Posicao.Linha - 1, Posicao.Coluna-1);
+                if (Tabuleiro.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+
+                Posicao.definirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+                if (Tabuleiro.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+            }
+            else
+            {
+                Posicao.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+                if (Tabuleiro.PosicaoValida(pos) && livre(pos))
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+
+
+                Posicao.definirValores(Posicao.Linha + 2, Posicao.Coluna);
+                if (Tabuleiro.PosicaoValida(pos) && livre(pos) && QuantidadeMovimentos == 0)
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+
+                Posicao.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+                if (Tabuleiro.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+
+                Posicao.definirValores(Posicao.Linha + 1, Posicao.Coluna +1);
+                if (Tabuleiro.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[Posicao.Linha, Posicao.Coluna] = true;
+                }
+            }
+            return mat;
+            
+        }
+    }
+}
